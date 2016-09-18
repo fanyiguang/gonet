1794802d1ac14d61884ff318e8e36a0e7fbae4b3
GameXG <GameXG@Gmail.Com>
2016-09-18 20:14:23 +0800

    pack 池 完工

---
+++ vnet/packpool.go
@@ -0,0 +1,122 @@
+package vnet
+
+const MaxPackSize = 1500
+
+type PackLayer int
+
+const (
+	PackLayerEthernet PackLayer = 1
+	PackLayerIP       PackLayer = 2
+)
+
+type Pack interface {
+
+	// 所有数据
+	// 为了减少内存拷贝，会预先保留二次封装包包头的空间，使得封包时不用再次拷贝。
+	GetAllData() []byte
+
+	// tun/tap 包数据
+	GetPrefixData() []byte
+	GetPackData() []byte
+	SetPackLength(int)
+
+	// 释放本包占用的内存
+	Free()
+
+	Reset()
+
+	GetPackLayer() PackLayer
+}
+
+type Freer interface {
+	Free(Pack)
+}
+
+type PackPool interface {
+	Malloc() Pack
+}
+
+type pack struct {
+	data         []byte
+	prefixSize   int
+	packDataSize int
+	free         Freer
+	packLayer    PackLayer
+}
+
+type packPool struct {
+	// 之前测试过，多线程时还是 chan 实现池效果最好
+	pool chan Pack
+	new  func() Pack
+}
+
+func (p *pack) GetPrefixData() []byte {
+	return p.data[:p.prefixSize]
+}
+func (p *pack) GetAllData() []byte {
+	return p.data[:p.prefixSize+p.packDataSize]
+}
+
+func (p *pack) GetPackData() []byte {
+	return p.data[p.prefixSize : p.prefixSize+p.packDataSize]
+}
+
+func (p *pack) SetPackLength(size int) {
+	p.packDataSize = size
+}
+
+func (p *pack) Free() {
+	if p.free != nil {
+		p.free.Free(p)
+	}
+}
+
+func (p *pack) GetPackLayer() PackLayer {
+	return p.packLayer
+}
+
+func (p *pack) Reset() {
+	p.packDataSize = len(p.data) - p.prefixSize
+}
+
+func newPack(data []byte, prefixSize int, packLayer PackLayer, free Freer) Pack {
+	p := &pack{
+		data:         data,
+		prefixSize:   prefixSize,
+		packDataSize: len(data) - prefixSize,
+		packLayer:    packLayer,
+		free:         free,
+	}
+	return p
+}
+
+func NewPool(prefixSize, MaxPackDataSize int, packLayer PackLayer) PackPool {
+	pool := &packPool{
+		pool: make(chan Pack, 100),
+	}
+
+	pool.new = func() Pack {
+		return newPack(make([]byte, prefixSize+MaxPackDataSize), prefixSize, packLayer, pool)
+	}
+
+	return pool
+}
+
+func (pool *packPool) Malloc() Pack {
+	var p Pack
+	select {
+	case p = <-pool.pool:
+	default:
+		p = pool.new()
+	}
+	p.Reset()
+	return p
+}
+
+func (pool *packPool) Free(p Pack) {
+	select {
+	case pool.pool <- p:
+	default:
+		return
+	}
+}
---
+++ vnet/packpool_test.go
@@ -0,0 +1,88 @@
+package vnet
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestPack(t *testing.T) {
+	pool := NewPool(10, MaxPackSize, PackLayerIP)
+	p := pool.Malloc()
+	if len(p.GetPrefixData()) != 10 {
+		t.Error("len(p.GetPrefixData())=", len(p.GetPrefixData()))
+	}
+	if len(p.GetPackData()) != MaxPackSize {
+		t.Error("len(p.GetPackData())=", len(p.GetPackData()))
+	}
+	if len(p.GetAllData()) != 10+MaxPackSize {
+		t.Error("len(p.GetAllData())=", len(p.GetAllData()))
+	}
+
+	pd := p.GetPrefixData()
+	for i, _ := range pd {
+		pd[i] = 123
+	}
+
+	for _, v := range p.GetPackData() {
+		if v == 123 {
+			t.Error(v)
+		}
+	}
+
+	p.SetPackLength(110)
+
+	if len(p.GetPrefixData()) != 10 {
+		t.Error("len(p.GetPrefixData())=", len(p.GetPrefixData()))
+	}
+	if len(p.GetPackData()) != 110 {
+		t.Error("len(p.GetPackData())=", len(p.GetPackData()))
+	}
+	if len(p.GetAllData()) != 10+110 {
+		t.Error("len(p.GetAllData())=", len(p.GetAllData()))
+	}
+
+	p.Free()
+	p2 := pool.Malloc()
+	if !bytes.Equal(p2.GetPrefixData(), p.GetPrefixData()) {
+		t.Error("p!=p2")
+	}
+	if len(p.GetPrefixData()) != 10 {
+		t.Error("len(p.GetPrefixData())=", len(p.GetPrefixData()))
+	}
+	if len(p.GetPackData()) != MaxPackSize {
+		t.Error("len(p.GetPackData())=", len(p.GetPackData()))
+	}
+	if len(p.GetAllData()) != 10+MaxPackSize {
+		t.Error("len(p.GetAllData())=", len(p.GetAllData()))
+	}
+}
+
+func TestPool(t *testing.T) {
+	pool := NewPool(10, MaxPackSize, PackLayerIP)
+
+	func() {
+		sTime := time.Now()
+		for i := 0; i < 1000000; i++ {
+			_ = pool.Malloc()
+		}
+		fmt.Println("单独申请执行耗时：", time.Now().Sub(sTime))
+	}()
+
+	func() {
+		sTime := time.Now()
+		for i := 0; i < 1000000; i++ {
+			p := pool.Malloc()
+			p.Free()
+		}
+		fmt.Println("申请+释放执行耗时：", time.Now().Sub(sTime))
+	}()
+}
+
+/*
+
+单独申请执行耗时： 713.287ms
+申请+释放执行耗时： 121.6307ms
+
+*/
