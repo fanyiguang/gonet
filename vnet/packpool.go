d10b21a870169044613073ee91ac480802191b0d
GameXG <GameXG@Gmail.Com>
2016-09-20 11:05:26 +0800

    简单 pack 解析

--- vnet/packpool.go
+++ vnet/packpool.go
@@ -1,12 +1,20 @@
 package vnet
 
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+)
+
 const MaxPackSize = 1500
 
 type PackLayer int
 
 const (
-	PackLayerEthernet PackLayer = 1
-	PackLayerIP       PackLayer = 2
+	PackLayerLink        PackLayer = 0x10 //以太网
+	PackLayerNetwork     PackLayer = 0x20 // ip
+	PackLayerTransport   PackLayer = 0x30 //tcp udp
+	PackLayerApplication PackLayer = 0x40 //
 )
 
 type Pack interface {
@@ -26,6 +34,10 @@ type Pack interface {
 	Reset()
 
 	GetPackLayer() PackLayer
+
+	// 非拷贝副本
+	GetIP() (net.IP, net.IP, error)
+	GetPort() (int, int, error)
 }
 
 type Freer interface {
@@ -80,6 +92,101 @@ func (p *pack) Reset() {
 	pd := p.GetPrefixData()
 	for i, _ := range pd {
 		pd[i] = 0
+	}
+}
+
+func (p *pack) GetIP() (sip, oip net.IP, rerr error) {
+	data, err := p.GetNetworkLayerData()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	switch data[0] & 0xf0 {
+	case 0x40:
+		// ipv4
+		if len(data) < 20 {
+			return nil, nil, fmt.Errorf("长度不正确")
+		}
+		return net.IP(data[12:16]), net.IP(data[16:20]), nil
+	case 0x60:
+		// ipv6 忽略
+		return nil, nil, fmt.Errorf("ipv6 未实现")
+	default:
+		return nil, nil, fmt.Errorf("未知协议 0x%X", data[0])
+	}
+}
+
+func (p *pack) GetNetworkLayerData() ([]byte, error) {
+	switch p.GetPackLayer() {
+	case PackLayerNetwork:
+		return p.GetPackData(), nil
+	default:
+		return nil, fmt.Errorf("不支持")
+	}
+}
+
+func (p *pack) GetTransportLayerData() (int, []byte, error) {
+	data := p.GetPackData()
+	packLayer := p.GetPackLayer()
+	if packLayer == PackLayerTransport {
+		return 0, data, nil
+	}
+
+	if packLayer < PackLayerNetwork {
+		ldata, err := p.GetNetworkLayerData()
+		if err != nil {
+			return 0, nil, err
+		}
+		data = ldata
+	} else if p.GetPackLayer() >= PackLayerApplication {
+		return 0, nil, fmt.Errorf("不支持")
+	}
+
+	switch data[0] & 0xf0 {
+	case 0x40:
+		// ipv4
+		if len(data) < 20 {
+			return 0, nil, fmt.Errorf("长度太短。")
+		}
+		headLen := int(data[0]&0x0f) * 4
+		allLen := binary.BigEndian.Uint16(data[2:4])
+		ptype := data[9]
+
+		if len(data) < int(allLen) {
+			return 0, nil, fmt.Errorf("长度太短")
+		}
+		return int(ptype), data[headLen:allLen], nil
+	case 0x60:
+		// ipv6 忽略
+		return 0, nil, fmt.Errorf("ipv6 未实现")
+	default:
+		return 0, nil, fmt.Errorf("未知协议 0x%X", data[0])
+	}
+}
+
+func (p *pack) GetPort() (sport, oport int, rerr error) {
+	ptype, data, err := p.GetTransportLayerData()
+	if err != nil {
+		return 0, 0, err
+	}
+	fmt.Println(data)
+
+	switch ptype {
+	case 17:
+		//udp
+		if len(data) < 4 {
+			return 0, 0, fmt.Errorf("长度不正确")
+		}
+		return int(binary.BigEndian.Uint16(data[0:2])), int(binary.BigEndian.Uint16(data[2:4])), nil
+
+	case 6:
+		//tcp
+		if len(data) < 4 {
+			return 0, 0, fmt.Errorf("长度不正确")
+		}
+		return int(binary.BigEndian.Uint16(data[0:2])), int(binary.BigEndian.Uint16(data[2:4])), nil
+	default:
+		return 0, 0, fmt.Errorf("未知 Transport 协议 %v", ptype)
 	}
 }
 
--- vnet/packpool_test.go
+++ vnet/packpool_test.go
@@ -3,12 +3,15 @@ package vnet
 import (
 	"bytes"
 	"fmt"
+	"net"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 )
 
 func TestPack(t *testing.T) {
-	pool := NewPool(10, MaxPackSize, PackLayerIP)
+	pool := NewPool(10, MaxPackSize, PackLayerNetwork)
 	p := pool.Malloc()
 	if len(p.GetPrefixData()) != 10 {
 		t.Error("len(p.GetPrefixData())=", len(p.GetPrefixData()))
@@ -71,7 +74,7 @@ func TestPack(t *testing.T) {
 }
 
 func TestPool(t *testing.T) {
-	pool := NewPool(10, MaxPackSize, PackLayerIP)
+	pool := NewPool(10, MaxPackSize, PackLayerNetwork)
 
 	func() {
 		sTime := time.Now()
@@ -97,3 +100,103 @@ func TestPool(t *testing.T) {
 申请+释放执行耗时： 121.6307ms
 
 */
+
+func ToBytes(d string) ([]byte, error) {
+	ss := strings.Split(d, " ")
+	r := make([]byte, 0, len(d)/3)
+	for _, s := range ss {
+		i, err := strconv.ParseUint(s, 16, 8)
+		if err != nil {
+			return nil, fmt.Errorf("解析错误，%v", err)
+		}
+
+		r = append(r, byte(i))
+	}
+	return r, nil
+}
+
+func ToPack(s string, t PackLayer) (Pack, error) {
+
+	d1, err := ToBytes(s)
+	if err != nil {
+		return nil, err
+	}
+
+	p := newPack(make([]byte, 2048), 3, t, nil)
+	copy(p.GetPackData(), d1)
+	p.SetPackLength(len(d1))
+
+	return p, nil
+}
+
+func TestPack_TCP(t *testing.T) {
+	s1 := `45 00 00 28 b8 b5 00 00 2d 06 33 23 dc ff 02 99 c0 a8 01 b7 01 bb cb c5 74 f7 b1 66 60 65 91 05 50 11 00 e5 27 ad 00 00 00 00 00 00 00 00`
+
+	p, err := ToPack(s1, PackLayerNetwork)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if p.GetPackLayer() != PackLayerNetwork {
+		t.Fatal("!=PackLayerNetwork")
+	}
+
+	sip, oip, err := p.GetIP()
+	if !sip.Equal(net.IPv4(220, 255, 2, 153)) || !oip.Equal(net.IPv4(192, 168, 1, 183)) {
+		t.Error(sip, oip, err)
+	}
+
+	sport, oport, err := p.GetPort()
+	if sport != 443 || oport != 52165 {
+		t.Error(sport, oport, err)
+	}
+
+}
+
+func TestPack_UDP(t *testing.T) {
+	s1 := `45 00 00 20 67 a5 00 00 80 11 eb 50 c0 a8 01 b7 2d 20 f8 57 27 09 27 09 00 0c 30 55 4b cc 4d cb`
+
+	p, err := ToPack(s1, PackLayerNetwork)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if p.GetPackLayer() != PackLayerNetwork {
+		t.Fatal("!=PackLayerNetwork")
+	}
+
+	sip, oip, err := p.GetIP()
+	if !sip.Equal(net.IPv4(192, 168, 1, 183)) || !oip.Equal(net.IPv4(45, 32, 248, 87)) {
+		t.Error(sip, oip, err)
+	}
+
+	sport, oport, err := p.GetPort()
+	if sport != 9993 || oport != 9993 {
+		t.Error(sport, oport, err)
+	}
+
+}
+
+func TestPack_DNS(t *testing.T) {
+	s1 := `45 00 00 3e 42 cc 00 00 80 11 73 da c0 a8 01 b7 c0 a8 01 01 e8 94 00 35 00 2a 84 8e 17 3c 01 00 00 01 00 00 00 00 00 00 05 69 6e 62 6f 78 06 67 6f 6f 67 6c 65 03 63 6f 6d 00 00 01 00 01`
+
+	p, err := ToPack(s1, PackLayerNetwork)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if p.GetPackLayer() != PackLayerNetwork {
+		t.Fatal("!=PackLayerNetwork")
+	}
+
+	sip, oip, err := p.GetIP()
+	if !sip.Equal(net.IPv4(192, 168, 1, 183)) || !oip.Equal(net.IPv4(192, 168, 1, 1)) {
+		t.Error(sip, oip, err)
+	}
+
+	sport, oport, err := p.GetPort()
+	if sport != 59540 || oport != 53 {
+		t.Error(sport, oport, err)
+	}
+
+}
