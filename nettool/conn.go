e91551108e28172e26c5bf53aa2c6b16e29fc335
GameXG <GameXG@Gmail.Com>
2016-10-14 16:08:06 +0800

    增加 conn 功能

---
+++ nettool/conn.go
@@ -0,0 +1,48 @@
+package httplog
+
+import (
+	"io"
+	"net"
+)
+
+type Conn struct {
+	net.Conn
+	r io.ReadCloser
+	w io.WriteCloser
+}
+
+func NewConn(conn net.Conn, r io.ReadCloser, w io.WriteCloser) *net.Conn {
+	return &Conn{
+		Conn: conn,
+		r:    r,
+		w:    w,
+	}
+}
+
+func (c *Conn) Read(p []byte) (n int, err error) {
+	if c.r != nil {
+		return c.r.Read(p)
+	}
+	return c.Conn.Read(p)
+}
+
+func (c *Conn) Write(p []byte) (n int, err error) {
+	if c.w != nil {
+		return c.w.Write(p)
+	}
+	return c.Conn.Write(p)
+}
+
+func (c *Conn) Close() error {
+	if c.r != nil {
+		c.r.Close()
+	}
+
+	if c.w != nil {
+		c.w.Close()
+	}
+
+	return c.Conn.Close()
+}
+
+
